d1c1ce50760f68402d13a3ffd2f32bb10362349d
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2019-06-15 16:31:11 +0530

    Create sherlockandthebeast.cpp

---
+++ Greedy/sherlockandthebeast.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the decentNumber function below.
+void decentNumber(int n) {
+
+    int temp=n;
+    while (temp%3!=0)
+    {
+        temp = temp-5;
+        if (temp<0)
+        {
+            cout<<-1<<endl;
+            return;
+        }
+    }
+
+    for (int i=0;i<temp;i++)
+    {
+        cout<<5;
+    }
+    for (int i=0;i<n-temp;i++)
+    {
+        cout<<3;
+    }
+
+    cout<<endl;
+
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        decentNumber(n);
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
